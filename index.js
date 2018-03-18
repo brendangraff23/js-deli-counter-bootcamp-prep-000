25ef349095483916f4a9aff56224327232d543d5
Brendan <brendangraff@gmail.com>
2018-03-18 21:22:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ function currentLine(katzDeliLine)
     text[i] = (i + 1) + '. ' + katzDeliLine[i];
   }
   
-  return `The line is currently: ${text.join(', \')`;
+  return `The line is currently: ${text.join(', ')`;
   }
 }
